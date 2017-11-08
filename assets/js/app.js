85d361da4644590dca1c046a19f87e64e9500b3b
Kdangelo <kdangelof@gmail.com>
2017-11-08 11:32:40 -0300

    agregando archivos al repositorio

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+(function() {
+	//variables
+	var list = document.getElementById("list"),
+		taskInput = document.getElementById("taskInput"),
+		btnNewTask = document.getElementById("btn-add");
+
+	//funciones
+	var addTask = function() {};
+
+	//eventos
+
+	//agregar tarea
+	btnNewTask.addEventListener("click", addTask);
+
+	//comprobar input
+
+	//borrar elementos de una lista
+	/*for (var i = 0; i < list.children.length -1; i++) {
+		list.children[i].addEventListener("click", removeTask);
+	}*/
+
+}());
